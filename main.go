a56325f1de555e6a1e193b271224c96090540727
gabriel <gabrielaxesm@gmail.com>
2026-04-07 17:13:42 -0300

    chore: learn to make a basic http server respond

--- main.go
+++ main.go
@@ -1,46 +1,23 @@
 package main
 
 import (
-    "flag"
-    "html/template"
-    "log"
-    "net/http"
+	"log"
+	"flag"
+	"net/http"
 )
 
 var addr = flag.String("addr", ":1718", "http service address") // Q=17, R=18
 
-var templ = template.Must(template.New("qr").Parse(templateStr))
-
 func main() {
-    flag.Parse()
-    http.Handle("/", http.HandlerFunc(QR))
-    err := http.ListenAndServe(*addr, nil)
-    if err != nil {
-        log.Fatal("ListenAndServe:", err)
-    }
+	http.Handle("/ping", http.HandlerFunc(Pong))
+	err := http.ListenAndServe(*addr, nil)
+	if err != nil {
+		log.Fatal("Some error ocurred: ", nil)
+	}
 }
 
-func QR(w http.ResponseWriter, req *http.Request) {
-    templ.Execute(w, req.FormValue("s"))
+func Pong(w http.ResponseWriter, req *http.Request) {
+	w.Write([]byte("pong"))
 }
 
-const templateStr = `
-<html>
-<head>
-<title>QR Link Generator</title>
-</head>
-<body>
-{{if .}}
-<img src="http://chart.apis.google.com/chart?chs=300x300&cht=qr&choe=UTF-8&chl={{.}}" />
-<br>
-{{.}}
-<br>
-<br>
-{{end}}
-<form action="/" name=f method="GET">
-    <input maxLength=1024 size=70 name=s value="" title="Text to QR Encode">
-    <input type=submit value="Show QR" name=qr>
-</form>
-</body>
-</html>
-`
+// type player struct
