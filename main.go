fa9a32c8cd89e41fdeac07503042a117ca83d657
gabriel <gabrielaxesm@gmail.com>
2026-04-07 17:15:59 -0300

    feat: make structs to hold a unit, the game and a player data

--- main.go
+++ main.go
@@ -9,6 +9,10 @@ import (
 var addr = flag.String("addr", ":1718", "http service address") // Q=17, R=18
 
 func main() {
+	// handle_http()
+}
+
+func handle_http() {
 	http.Handle("/ping", http.HandlerFunc(Pong))
 	err := http.ListenAndServe(*addr, nil)
 	if err != nil {
@@ -20,4 +24,18 @@ func Pong(w http.ResponseWriter, req *http.Request) {
 	w.Write([]byte("pong"))
 }
 
-// type player struct
+type unit struct {
+	name string
+	health int
+	damage int
+}
+
+type player struct {
+	name string
+	main_unit unit
+}
+
+type game struct {
+	players []player // TODO: Make this hold exactly 2
+	turn int
+}
