cfacbdb09c7ebdfdfd3ca5f0976a154bcab652d1
gabriel <gabrielaxesm@gmail.com>
2026-05-22 21:05:58 -0300

    feat: make structs to combat requests

--- structs.go
+++ structs.go
@@ -23,14 +23,11 @@ type game struct {
 	// NOTE: I still dont know why i need to put these Uppercased
 }
 
-func make_goblin() *unit {
-	return &unit{name: "Goblin", health: 70, damage: 10}
+type AttackRequest struct {
+	Unit_id *int  `json:"unit_id"`  
+	Target_id *int `json:"target_id"`
 }
 
-func make_werewolf() *unit {
-	return &unit{name: "Werewolf", health: 130, damage: 30}
-}
-
-func make_elven() *unit {
-	return &unit{name: "Elven", health: 100, damage: 20}
+type DefenseRequest struct {
+	Unit_id *int  `json:"unit_id"`  
 }
