372cfc09746f7ae771e9a6dce33db17f70220218
gabriel <gabrielaxesm@gmail.com>
2026-05-24 22:47:23 -0300

    chore: rename file

--- effects.go
+++ effects.go
@@ -15,15 +15,34 @@ type PoisonEffect struct {
 	Turns int
 }
 
+type FireEffect struct {
+	DamagePerTurn int
+	Turns int
+}
+
 var AttackPoisonSting = Attack {
 	Name: "Poison Sting",
+	Damage: 5,
+	Effect: PoisonEffect{DamagePerTurn: 3, Turns: 7},
+}
+
+var AttackFireball = Attack {
+	Name: "Fireball",
 	Damage: 10,
-	Effect: PoisonEffect{DamagePerTurn: 5, Turns: 3},
+	Effect: FireEffect{DamagePerTurn: 5, Turns: 3},
 }
 
 func (p PoisonEffect) Apply(target *Unit, attacker *Unit) {
 	target.Effects = append(target.Effects, StatusEffect{
 		Type: "poison",
+		Damage: p.DamagePerTurn,
+		Duration: p.Turns,
+	})
+}
+
+func (p FireEffect) Apply(target *Unit, attacker *Unit) {
+	target.Effects = append(target.Effects, StatusEffect{
+		Type: "fire",
 		Damage: p.DamagePerTurn,
 		Duration: p.Turns,
 	})
