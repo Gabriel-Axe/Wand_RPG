5956974e776e2c05209014b334d83ac6b452c458
gabriel <gabrielaxesm@gmail.com>
2026-05-29 11:46:28 -0300

    feat: create a factory for mist potions

--- items.go
+++ items.go
@@ -15,3 +15,22 @@ func NewWand(name string, spells[]Attack, manaPool int, rechargeRate int) Wand {
 		RechargeRate: rechargeRate,
 	}
 }
+
+func NewMistPotion() Potion {
+
+	f := FreezeEffect{
+		Slowdown: 3,
+		Turns: 4,
+	}
+	i := FireEffect{
+		DamagePerTurn: 5,
+		Turns: 4,
+	}
+
+	return  Potion{
+		Effects: []StatusEffect{
+			f.ToStatusEffect(),
+			i.ToStatusEffect(),
+		},
+	}
+}
